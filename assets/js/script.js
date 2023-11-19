2c00f185ee2ce9222314464594c39eb9cbc0ca5c
benitaisaac <benita.isaac@gmail.com>
2023-11-18 16:00:10 -0800

    added functional timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
-var startEl = document.querySelector("#start");
+var startButton = document.querySelector("#start");
+var timerElement = document.querySelector(".timer-count");
 
-startEl.addEventListener("click", function() {
-    console.log("you've started the quiz");
-    
-})
+var timer;
+var timerCount;
+
+function startGame(){
+    startButton.addEventListener("click", function() {
+        console.log("you've started the quiz");
+        timerCount = 60
+        startTimer();
+    })
+}
+
+function startTimer() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function (){
+        timeLeft--;
+        timerElement.textContent = timeLeft + " seconds left";
+
+    if (timeLeft === 0){
+        clearInterval(timeInterval); 
+    }
+    }, 1000);
+}
+
+startGame();
