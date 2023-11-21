9aef4ba9096b973d40b1699daf88f35a0d2bdc66
benitaisaac <benita.isaac@gmail.com>
2023-11-21 11:52:55 -0800

    Major changes. 1) changed styling of buttons to look nicer, using resources online. 2) edited html to add new divs to support formatting in html. 3) added a new button for 'next' 4) commented out most of javascript code in order to start again, using for loops 5) created a new array in javascript holding questions, answers and boolean variable 'correct' git status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,66 @@
-var startButton = document.querySelector("#start");
+var startButton = document.querySelector(".start");
 var timerElement = document.querySelector(".timer-count");
+var answerBtn = document.querySelector(".answer");
 
-let questionOne = ["text for question one", "answer option 1_1", "answer option 2_1", "answer option 3_1", "answer option 4_1", "answer option 1_1", ];
-let questionTwo = ["text for question two", "answer option 1_2", "answer option 2_2", "answer option 3_2", "answer option 4_2", "answer option 5_2"];
-let questionThree = ["text for question three", "answer option 1_3", "answer option 2_3", "answer option 3_3", "answer option 4_3", "answer option 5_3"];
-let questionFour = ["text for question four", "answer option 1_4", "answer option 2_4", "answer option 3_4", "answer option 4_4", "answer option 5_4"];
-let questionFive = ["text for question five", "answer option 1_5", "answer option 2_5", "answer option 3_5", "answer option 4_5", "answer option 5_5"];
+const questions = [
+    {
+        question: "This is question 1",
+        answers: [
+            {text: "answer option 1", correct: true},
+            {text: "answer option 2", correct: false},
+            {text: "answer option 3", correct: false},
+            {text: "answer option 4", correct: false}
+        ],
 
-let userSelection = {};
+        question: "This is question 2",
+        answers: [
+            {text: "answer option 1", correct: false},
+            {text: "answer option 2", correct: true},
+            {text: "answer option 3", correct: false},
+            {text: "answer option 4", correct: false}
+        ],
+
+        question: "This is question 3",
+        answers: [
+            {text: "answer option 1", correct: true},
+            {text: "answer option 2", correct: false},
+            {text: "answer option 3", correct: false},
+            {text: "answer option 4", correct: false}
+        ],
+        
+        question: "This is question 4",
+        answers: [
+            {text: "answer option 1", correct: false},
+            {text: "answer option 2", correct: false},
+            {text: "answer option 3", correct: false},
+            {text: "answer option 4", correct: true}
+        ]
+    },
+]
+
+// let questionOne = ["text for question one", "answer option 1_1", "answer option 2_1", "answer option 3_1", "answer option 4_1", "answer option 1_1", ];
+// let questionTwo = ["text for question two", "answer option 1_2", "answer option 2_2", "answer option 3_2", "answer option 4_2", "answer option 5_2"];
+// let questionThree = ["text for question three", "answer option 1_3", "answer option 2_3", "answer option 3_3", "answer option 4_3", "answer option 5_3"];
+// let questionFour = ["text for question four", "answer option 1_4", "answer option 2_4", "answer option 3_4", "answer option 4_4", "answer option 5_4"];
+// let questionFive = ["text for question five", "answer option 1_5", "answer option 2_5", "answer option 3_5", "answer option 4_5", "answer option 5_5"];
+
+// let userSelection = { userAnswer1: " "};
 
 var timer;
 var timerCount;
+timerCount = 60;
+
 
 //This function starts the game once the user presses the 'start' button on the webpage
 //This function will...
 //1. Start the timer 2. Display the hidden buttons 3. Display content on the buttons 
-function startGame(){ 
-    timerCount = 60
-    startTimer();
-    buttonsVisible();
-    buttonText1();
-    userClick();
-}
+// function startGame(){ 
+//     timerCount = 60
+//     startTimer();
+//     buttonsVisible();
+//     buttonText1();
+//     userClick()
+// }
 
 function startTimer() {
     var timeLeft = 60;
@@ -35,30 +74,27 @@ function startTimer() {
     }, 1000);
 }
 function buttonsVisible(){
-    answer1.setAttribute("style", " visibility: visible");
-    answer2.setAttribute("style", " visibility: visible");
-    answer3.setAttribute("style", " visibility: visible");
-    answer4.setAttribute("style", " visibility: visible");
+    answerBtn.setAttribute("style", " visibility: visible");
 }
 
-function buttonText1(){
-    big_text.textContent = questionOne[0];
-    answer1.textContent = questionOne[1];
-    answer2.textContent = questionOne[2];
-    answer3.textContent = questionOne[3];
-    answer4.textContent = questionOne[4];
-}
+// function buttonText1(){
+//     big_text.textContent = questionOne[0];
+//     answer1.textContent = questionOne[1];
+//     answer2.textContent = questionOne[2];
+//     answer3.textContent = questionOne[3];
+//     answer4.textContent = questionOne[4];
+//     start.textContent = "Submit"
+// }
 
-//Function that checks to see which answer the user clicked and send that information to userSelection
-function userClick(){
-    answer1.addEventListener("click", () => (userSelection.userAnswer1 = answer1.textContent));
-    answer2.addEventListener("click", () => (userSelection.userAnswer1 = answer2.textContent));
-    answer3.addEventListener("click", () => (userSelection.userAnswer1 = answer3.textContent));
-    answer4.addEventListener("click", () => (userSelection.userAnswer1 = answer4.textContent));
-    console.log(userSelection);
-    console.log(userSelection.userAnswer1)
-    // checkAnswer();
-}
+// //Function that checks to see which answer the user clicked and send that information to userSelection
+// function userClick(){
+//     answer1.addEventListener("click", () => (userSelection.userAnswer1 = answer1.textContent));
+//     answer2.addEventListener("click", () => (userSelection.userAnswer1 = answer2.textContent));
+//     answer3.addEventListener("click", () => (userSelection.userAnswer1 = answer3.textContent));
+//     answer4.addEventListener("click", () => (userSelection.userAnswer1 = answer4.textContent));
+//     checkAnswer();
+//     console.log(userSelection) 
+//  }
 
 // function checkAnswer(){
 //     if (userSelection.userAnswer1 === questionOne[5]){
@@ -66,4 +102,6 @@ function userClick(){
 //     }
 // }
 
-startButton.addEventListener("click",() => startGame());
+startButton.addEventListener("click",() => startTimer());
+
+
